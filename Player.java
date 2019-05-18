ceda1ec170b4ed9cddef757e5046b845a75c30bf
833529 <50774670+833529@users.noreply.github.com>
2019-05-18 15:32:59 -0400

    Update Player.java

--- Player.java
+++ Player.java
@@ -1,39 +1,87 @@
+
 import java.awt.Color;
+import java.awt.Graphics;
 
 public class Player extends GravityBall{
-
-	private double jumpHeight;
-	
-	
-	public Character(double x, double y, double diameter, Color color) {
+	private double acceleration;
+	private double diameter;
+	private double radius;
+	/**
+	 * Creates a GravityBall with center at x,y with a diameter and color
+	 * that the user inputs
+	 * @param x where the middle of the GravityBall is in the horizontal position
+	 * @param y where the middle of the GravityBall is in the vertical position
+	 * @param diameter the diameter of the ball
+	 * @param color the color of the ball
+	 */
+	public Player(double x, double y, double diameter, Color color) {
 		super(x,y,diameter,color);
+		this.diameter=diameter;
+		this.radius=diameter/2;
 	}
-	
-	public void jump() {
-		setYSpeed(getJumpHeight());
+	/**
+	 * Sets the acceleration at what the user inputs
+	 * @param acceleration the acceleration of the ball
+	 */
+	public void setAcceleration(double acceleration) {
+		this.acceleration = acceleration;
 	}
-	
-	public void move(double deltaTime) {
+	/**
+	 * returns the acceleration
+	 */
+	public double getAcceleration() {
+		return acceleration;
+	}
+	/**
+	 * sets the initial velocity of the GravityBall
+	 * @param speed the speed that the GravityBall is launched at
+	 * @param angle the angle that the GravityBall is launched at
+	 */
+	public void setInitialVelocity(int speed, int angle) {
+		setXSpeed(speed*Math.cos(Math.toRadians(angle)));
+		setYSpeed(speed*(-1)*Math.sin(Math.toRadians(angle)));
+	}
+	/**
+	 * Launches GravityBall, bounces off the sides of the screen
+	 * @param rightEdge the right edge of the window
+	 * @param bottomEdge the bottom edge of the window
+	 * @param deltaTime the change in time
+	 */
+	public void Launch(int rightEdge, int bottomEdge, double deltaTime, DoodleBumper bumper) {
 		setXSpeed(getXSpeed());
-		setYSpeed(getYSpeed() + (getAcceleration()*deltaTime));
+		setYSpeed(getYSpeed()+getAcceleration()*deltaTime);
 		
-		setX(getX() + getXSpeed()*(deltaTime/1000));
-		setY(getY() + getYSpeed()*(deltaTime/1000));
+		setX(getX()+getXSpeed()*deltaTime/1000);
+		setY(getY()+getYSpeed()*deltaTime/1000);
+		if(getX()+radius>=rightEdge) {
+			setXSpeed(getXSpeed()*(-1));
+			setX(rightEdge-radius);
+			//setRandomColor();
+		}
+		if(getX()-radius<=0) {
+			setXSpeed(getXSpeed()*(-1));
+			setX(radius);
+			//setRandomColor();
+		}
+		if(getY()+radius>=bottomEdge) {
+			setYSpeed(getYSpeed()*(-1));
+			setY(bottomEdge-radius);
+			//setRandomColor();
+		}
+		if(getY()+radius>=bumper.getY()&&getX()+radius>=bumper.getX()&&getX()-radius<=bumper.getX()+bumper.getWidth()) {
+			setYSpeed(getYSpeed()*(-1));
+			setY(bumper.getY()-radius);
+		}
+		if(getY()-radius<=0) {
+			setYSpeed(getYSpeed()*(-1));
+			setY(radius);
+			//setRandomColor();
+		}
 	}
-
 	/**
-	 * @return the jumpHeight
+	 * draws the GravityBall
 	 */
-	public double getJumpHeight() {
-		return jumpHeight;
+	public void draw(Graphics g) {
+		super.draw(g);
 	}
-
-	/**
-	 * @param jumpHeight the jumpHeight to set
-	 */
-	public void setJumpHeight(double jumpHeight) {
-		this.jumpHeight = jumpHeight;
-	}
-	
-	
 }
