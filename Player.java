79c7df9895bb918aafcef6ba4ffba1e984cfe4a8
815910 <50774639+815910@users.noreply.github.com>
2019-05-18 14:27:23 -0400

    Add Player class

---
+++ Player.java
@@ -0,0 +1,39 @@
+import java.awt.Color;
+
+public class Player extends GravityBall{
+
+	private double jumpHeight;
+	
+	
+	public Character(double x, double y, double diameter, Color color) {
+		super(x,y,diameter,color);
+	}
+	
+	public void jump() {
+		setYSpeed(getJumpHeight());
+	}
+	
+	public void move(double deltaTime) {
+		setXSpeed(getXSpeed());
+		setYSpeed(getYSpeed() + (getAcceleration()*deltaTime));
+		
+		setX(getX() + getXSpeed()*(deltaTime/1000));
+		setY(getY() + getYSpeed()*(deltaTime/1000));
+	}
+
+	/**
+	 * @return the jumpHeight
+	 */
+	public double getJumpHeight() {
+		return jumpHeight;
+	}
+
+	/**
+	 * @param jumpHeight the jumpHeight to set
+	 */
+	public void setJumpHeight(double jumpHeight) {
+		this.jumpHeight = jumpHeight;
+	}
+	
+	
+}
