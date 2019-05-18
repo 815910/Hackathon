6f79862bdb2fa2ec0056a279b04550690f4cdfc2
815910 <50774639+815910@users.noreply.github.com>
2019-05-18 19:02:20 -0400

    Final Change

--- Team29.java
+++ Team29.java
@@ -11,6 +11,7 @@ import java.awt.event.MouseMotionListener;
 import java.awt.event.MouseWheelEvent;
 import java.awt.event.MouseWheelListener;
 import java.awt.image.BufferedImage;
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -27,15 +28,17 @@ public class Team29 extends JPanel {
 
 	private static final int WIDTH = 850;
 	private static final int HEIGHT = 1100;
-	private static final int playerSpeed = 200;
-	private static final int bumperSpeed = 5;
+	private static final int playerSpeed = 50;
+	private static final int bumperSpeed = 25;
 	private static final Color LIGHT_BLUE = new Color(108, 210, 247);
-	private static final int deltaTime = 10;
+	private static final int deltaTime = 7;
 
 	private BufferedImage image;
 	private Graphics g;
 	private Timer timer;
 	private int ticks;
+	private double score;
+	private double bestY = 600;
 
 	private Player player;
 	private List<DoodleBumper> DoodleBumpers;
@@ -46,12 +49,12 @@ public class Team29 extends JPanel {
 		DoodleBumpers = new ArrayList<DoodleBumper>();
 		for(int i = 0; i < 5; i++) {
 			DoodleBumpers.add(new DoodleBumper((int)(Math.random()*800+100), (int)(Math.random()*400+250)));
-			DoodleBumpers.get(i).setSpeed(bumperSpeed);
+			
 		}
 
 		System.out.println(DoodleBumpers.get(0).getX());
 		player = new Player(WIDTH/2,HEIGHT-250,50,Color.green.darker());
-		player.setInitialVelocity(playerSpeed,90);
+		player.setInitialVelocity(bumperSpeed,90);
 		image = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_RGB);
 		g = image.getGraphics();
 
@@ -74,6 +77,44 @@ public class Team29 extends JPanel {
 			player.setXSpeed(0);
 		}
 	}
+	
+	public Boolean checkDie() {
+		if(player.getY()>HEIGHT) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	
+	public void end() {
+		if(checkDie()) {
+			g.setColor(Color.BLACK);
+			g.setFont(new Font("Tahoma", Font.BOLD, 40));
+			g.drawString("Game Over", WIDTH/2, HEIGHT/2-HEIGHT/3);
+			timer.stop();
+		}
+	}
+	
+public void printInfo() {
+		
+		
+		
+		g.setColor(Color.BLACK);
+		g.fillRect(0, 0, 250, 100);
+		g.setColor(Color.BLACK);
+		g.fillRect(1350, 0, 500, 100);
+		g.setColor(Color.WHITE);
+		g.setFont(new Font("Tahoma", Font.BOLD, 30));
+		score = (ticks/10 );
+		
+		String pattern = "#.##";
+		DecimalFormat numberFormat = new DecimalFormat (pattern);
+		g.drawString("Score: " + numberFormat.format(score) + " " , 10, 40);
+		
+		
+	}
+
 
 	private class Keyboard implements KeyListener {
 		public void keyPressed(KeyEvent e) {
@@ -112,24 +153,32 @@ public class Team29 extends JPanel {
 			// draw background / clear screen
 			GraphicsUtilities.drawBackground(g, LIGHT_BLUE, WIDTH, HEIGHT);
 			
-			if(ticks%30 == 0) {
+			if(ticks%15 == 0) {
 				DoodleBumpers.add(new DoodleBumper((int)(Math.random()*800+100), -200));
-				DoodleBumpers.get(DoodleBumpers.size()-1).setSpeed(bumperSpeed);
+			}
+			end();
+			
+			if(player.getY() < bestY) {
+				for(int i = 0; i < DoodleBumpers.size(); i++) {
+					DoodleBumpers.get(i).move(HEIGHT, WIDTH, (int)((bestY - player.getY())/50));
+				}
 			}
 			
 			for(int i = 0; i < DoodleBumpers.size(); i++) {
-				DoodleBumpers.get(i).move(HEIGHT, WIDTH);
-				if(DoodleBumpers.get(i).getY()>HEIGHT) {
-					DoodleBumpers.remove(i);
-				}
-				player.Launch(WIDTH, HEIGHT, deltaTime, DoodleBumpers.get(i));
+//				DoodleBumpers.get(i).move(HEIGHT, WIDTH);
+//				if(DoodleBumpers.get(i).getY()>HEIGHT) {
+//					DoodleBumpers.remove(i);
+//				}
+				player.Launch(WIDTH, HEIGHT, deltaTime, DoodleBumpers.get(i),bumperSpeed,90);
 			}
 
 			player.draw(g);
 			for(int i = 0; i < DoodleBumpers.size(); i++) {
 				DoodleBumpers.get(i).draw(g);
 			}
+			
 			updateMovement();
+			printInfo();
 			System.out.println(player.getYSpeed());
 			ticks++;
 			repaint();
